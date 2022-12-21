2c08508d55185184633514407f01d6f1c0e4a808
Eduardo Camacho Díaz <fgada200@gmail.com>
2022-12-21 12:57:16 +0000

    Feature: Added more icons, and a dynamic hour display. It updates the hour

--- components/InfoBar.jsx
+++ components/InfoBar.jsx
@@ -1,23 +1,28 @@
 import { View, Text, StyleSheet, ImageBackground, Image } from "react-native";
+import { useState } from "react";
 
 const InfoBar = () => {
-    const horas = new Date().getHours();
-    const minutos = new Date().getMinutes();
-    console.log(horas);
-    console.log(minutos);
+    const [hours, setHours] = useState(new Date().getHours());
+    const [minutes, setMinutes] = useState(new Date().getMinutes());
+
+    setInterval(() => {
+        setHours(new Date().getHours());
+        setMinutes(new Date().getMinutes());
+    }, 1000);
+
     return (
         <View style={styles.infoBarContainer}>
             <ImageBackground source={require("../assets/images.jpeg")} style={styles.image}>
                 <View style={styles.leftIcons}>
-                    <Image source={require("../assets/cube.png")} style={{ width: 24, height: 24 }} />
+                    <Image source={require("../assets/cube.png")} style={styles.iconStyle} />
                 </View>
                 <View style={styles.rightIcons}>
-                    <Image source={require("../assets/shield.png")} style={{ width: 24, height: 24 }} />
-                    <Image source={require("../assets/bluetooth.png")} style={{ width: 24, height: 24 }} />
-                    <Text style={{ color: "white", marginRight: 5 }}>icono</Text>
-                    <Text style={{ color: "white", marginRight: 5 }}>icono</Text>
+                    <Image source={require("../assets/shield.png")} style={styles.iconStyle} />
+                    <Image source={require("../assets/bluetooth.png")} style={styles.iconStyle} />
+                    <Image source={require("../assets/person.png")} style={styles.iconStyle} />
+                    <Image source={require("../assets/lte.png")} style={styles.iconStyle} />
                     <Text style={{ color: "white" }}>
-                        {horas} : {minutos}
+                        {hours} : {minutes}
                     </Text>
                 </View>
             </ImageBackground>
@@ -51,6 +56,12 @@ const styles = StyleSheet.create({
         justifyContent: "flex-end",
         alignItems: "center",
         flexDirection: "row",
+    },
+
+    iconStyle: {
+        width: 24,
+        height: 24,
+        marginRight: 5,
     },
 });
 
